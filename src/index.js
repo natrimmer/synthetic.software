be39cc89b042e51e1df109dbae002735a006041a
natrimmer <nicholas.trimmer@mcgovern.org>
2025-11-11 16:47:25 -0800

    refactor: remove hugo build tools and queue system

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// src/index.js - Minimal Worker for static assets
-export default {
-  async fetch(request, env) {
-    // Serve static assets for all requests
-    return env.ASSETS.fetch(request);
-  },
-};
