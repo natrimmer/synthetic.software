053ed1c0bfe38f3f849514593431f89273cdb061
natrimmer <nicholas.trimmer@mcgovern.org>
2026-02-27 14:50:10 -0800

    refactor: convert generic error page to dedicated 404 page

--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
-	import { page } from '$app/state';
 </script>
 
 <div class="flex flex-1 flex-col items-center justify-center text-center font-mono">
-	<h1 class="text-[20vh] font-bold">{page.status}</h1>
-	<p class="text-lg italic">{page.error?.message}</p>
+	<h1 class="text-[20vh] font-bold">404</h1>
+	<p class="text-lg italic">Not Found</p>
 	<a href={resolve('/')} class="underline hover:no-underline">go home</a>
 </div>
